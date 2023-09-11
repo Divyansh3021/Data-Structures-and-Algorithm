fddccd82c05f86fae2d5e62624c1f5871e73fca1
Divyansh3021 <divyansh3021@gmail.com>
2023-09-11 08:15:13 +0530

    11 Sep 2023

--- Binary Search/Search2Dmatrix.py
+++ Binary Search/Search2Dmatrix.py
@@ -1,12 +1,15 @@
-matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
-target = 3
+matrix = [[1]]
+target = 2
 
 end_int = []
-for i in len(matrix):
-    end_int.append(matrix[i][len(m[i]-1)])
+for i in range(len(matrix)):
+    end_int.append(matrix[i][len(matrix[i])-1])
 
 left = 0
 right = len(end_int) - 1
+
+if end_int[-1] < target:
+    print(False)
 
 while left <= right:
     mid = left + ((right-left)//2)
@@ -19,3 +22,25 @@ while left <= right:
     
     else:
         print(True)
+        break
+
+sub_array = left
+print(f"Left: {left} Right: {right}")
+
+left = 0
+right = len(matrix[sub_array]) -1
+
+while left <= right:
+    mid = left + ((right-left)//2)
+
+    if matrix[sub_array][mid] > target:
+        right = mid - 1
+
+    elif matrix[sub_array][mid] < target:
+        left = mid + 1
+
+    else:
+        print(True)
+
+else:
+    print(False)
