cf8865573807997b67e9587fa1d593d5ba6939c2
Divyansh3021 <divyansh3021@gmail.com>
2023-10-29 13:13:46 +0530

    29 Oct 2023

---
+++ Tree/SumRoottoLeaf.py
@@ -0,0 +1,34 @@
+#-------Problem: 129--------
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, sum):
+            if not node:
+                return 0
+            
+            sum = sum * 10 + node.val
+            if not node.left and not node.right:
+                return sum
+            return (dfs(node.left, sum) + dfs(node.right, sum))
+        return dfs(root, 0)
+
+
+"""given tree 
+            1
+           / \
+          2   3
+
+    dfs(1,0)
+        sum = 1
+        dfs(2,1) 
+            sum = 12
+        dfs(3,1)
+            sum = 13
+        returns 25
+"""
