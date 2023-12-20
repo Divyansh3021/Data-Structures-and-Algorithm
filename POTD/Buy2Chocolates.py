980afed4013f1695b4ea23951b2408229b3fe7cc
Divyansh3021 <divyansh3021@gmail.com>
2023-12-20 17:35:09 +0530

    20 Dec 2023

---
+++ POTD/Buy2Chocolates.py
@@ -0,0 +1,8 @@
+#----------------------Problem: 2706-----------------------
+
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        prices = sorted(prices)
+        if prices[0] + prices[1] <= money:
+            return money - prices[0] - prices[1]
+        return money
