0e215231511e4b66b62fc44ae860d4ff360ccb8c
Divyansh3021 <divyansh3021@gmail.com>
2023-12-21 19:56:12 +0530

    21 Dec 2023

---
+++ POTD/WidestVerticalArea.py
@@ -0,0 +1,9 @@
+#--------------Problem:1637---------------
+
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        points = sorted(points)
+        res = 0
+        for i in range(len(points) - 1):
+            res = max(res, points[i+1][0] - points[i][0] )
+        return res
