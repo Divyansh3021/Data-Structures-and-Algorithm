9a7523135030a889571254104c63ed46967e0d7b
Divyansh3021 <divyansh3021@gmail.com>
2023-12-21 09:26:22 +0530

    21 Dec 2023

---
+++ 1-D Dynamic Programming/MinCostForTickets.py
@@ -0,0 +1,22 @@
+#----------------Problem: 983------------------
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dp = {}
+
+        def dfs(i):
+            if i == len(days):
+                return 0
+            if i in dp:
+                return dp[i]
+            
+            dp[i] = float("inf")
+            for day, cost in zip([1,7,30], costs):
+                j=i
+                while j < len(days) and days[j] < days[i] + day:
+                    j += 1
+                dp[i] = min(dp[i], cost + dfs(j))
+            
+            return dp[i]
+        
+        return dfs(0)
