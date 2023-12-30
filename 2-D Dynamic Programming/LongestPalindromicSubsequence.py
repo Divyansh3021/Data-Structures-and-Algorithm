dd6237649188a2051405b3b1655a96c334f4b05a
Divyansh3021 <divyansh3021@gmail.com>
2023-12-30 14:13:45 +0530

    30 Dec 2023

---
+++ 2-D Dynamic Programming/LongestPalindromicSubsequence.py
@@ -0,0 +1,16 @@
+#---------------Problem: 516------------------
+
+class Solution:
+    def longestPalindromeSubseq(self, text1: str) -> int:
+        text2 = text1[::-1]
+
+        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        
+        return dp[0][0]
