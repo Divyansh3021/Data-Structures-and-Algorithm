95f0e9e3e2eba087fa3b9db6ee97de81091f872f
Divyansh3021 <divyansh3021@gmail.com>
2024-01-02 20:00:09 +0530

    2 Jan 2024

---
+++ POTD/ArrayTo2DArray.py
@@ -0,0 +1,23 @@
+#----------------Problem: 2610------------------
+
+class Solution:
+    def findMatrix(self, nums):
+        ans = []
+        s = set()
+
+        while nums:
+            uniqueRow = []
+
+            i = 0
+            while i < len(nums):
+                if nums[i] not in s:
+                    s.add(nums[i])
+                    uniqueRow.append(nums[i])
+                    nums.pop(i)
+                    i -= 1
+                i += 1
+
+            ans.append(uniqueRow)
+            s.clear()
+
+        return ans
