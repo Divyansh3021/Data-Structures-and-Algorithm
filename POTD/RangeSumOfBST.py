97f217058f61b7d3c3f6ebc071faf8d0e29b7589
Divyansh3021 <divyansh3021@gmail.com>
2024-01-08 22:40:11 +0530

    8 Jan 2024

---
+++ POTD/RangeSumOfBST.py
@@ -0,0 +1,22 @@
+#---------------Problem: 938-----------------
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        sum = 0
+        stack = []
+        cur = root
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            if cur.val in range(low, high+1):
+                sum += cur.val
+            cur = cur.right
+        return sum
