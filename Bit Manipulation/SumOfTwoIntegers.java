59a7c10faece807b2350560cc0e4ab46462d5a44
Divyansh3021 <divyansh3021@gmail.com>
2024-02-26 11:38:28 +0530

    26 Feb 2024

---
+++ Bit Manipulation/SumOfTwoIntegers.java
@@ -0,0 +1,13 @@
+//----------------Problem: 371------------------
+
+class SumOfTwoIntegers {
+    public int getSum(int a, int b) {
+        int temp;
+        while (b!=0){
+            temp = (a&b) << 1;
+            a = a ^ b;
+            b = temp;
+        }
+        return a;
+    }
+}
