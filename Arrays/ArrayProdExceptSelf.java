40f895c5fb5d900147061599dda28a1e7b80a17f
Divyansh3021 <divyansh3021@gmail.com>
2024-03-04 12:56:37 +0530

    4 March 2024

---
+++ Arrays/ArrayProdExceptSelf.java
@@ -0,0 +1,19 @@
+//-------------Problem: 238-------------
+
+class ArrayProdExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int prefix=1;
+        int postfix = 1;
+        for(int i = 0; i < nums.length; i++){
+            res[i] = prefix;
+            prefix = prefix * nums[i];
+        }
+
+        for(int j = nums.length - 1; j >= 0; j--){
+            res[j] *= postfix;
+            postfix *= nums[j];
+        }
+        return res;
+    }
+}
